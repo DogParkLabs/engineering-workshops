4fd8cb42f077655f69ea7c672e6c930300536101
erik-ekberg-gd <102057982+erik-ekberg-gd@users.noreply.github.com>
2023-04-10 13:17:58 -0700

    Scaffold out tutorial

---
+++ terraform-aws-ecs-tutorial/main.tf
@@ -0,0 +1,27 @@
+# * Part 1 - Setup.
+locals {
+	container_name = "hello-world-container"
+	container_port = 8080 # ! Must be same port from our Dockerfile that we EXPOSE
+	example = "TODO_REPLACE_WITH_YOUR_NAME"
+}
+
+provider "aws" {
+	region = "ca-central-1"
+
+	default_tags {
+		tags = { example = local.example }
+	}
+}
+
+# * Give Docker permission to pusher Docker Images to AWS.
+data "aws_caller_identity" "this" {}
+data "aws_ecr_authorization_token" "this" {}
+data "aws_region" "this" {}
+locals { ecr_address = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.this.name) }
+provider "docker" {
+	registry_auth {
+		address  = local.ecr_address
+		password = data.aws_ecr_authorization_token.this.password
+		username = data.aws_ecr_authorization_token.this.user_name
+	}
+}
