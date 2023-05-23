6912fc1af9e6d3cd9d7c3271726960cecfe75ce6
erik-ekberg-gd <102057982+erik-ekberg-gd@users.noreply.github.com>
2023-05-23 16:03:37 -0700

    Write fizz buzz tests

--- ruby-rspec-tutorial/services/fizz_buzz.rb
+++ ruby-rspec-tutorial/services/fizz_buzz.rb
@@ -5,6 +5,9 @@ module Services
   # * is divisible by 3, Buzz when the number is divisible
   # * by 5, and FizzBuzz when divisible by both 3 and 5.
   class FizzBuzz
-    def self.call; end
+    def self.call(number)
+      Fizz.call(number)+
+        Buzz.call(number)
+    end
   end
 end
--- ruby-rspec-tutorial/spec/services/fizz_buzz_spec.rb
+++ ruby-rspec-tutorial/spec/services/fizz_buzz_spec.rb
@@ -1,3 +1,43 @@
 # frozen_string_literal: true
 
 require_relative '../../services/fizz_buzz'
+
+describe Services::FizzBuzz do
+  subject(:call) { described_class.call(number) }
+
+  context 'when #number is divisible by 3' do
+    context 'when #number is divisible by 5' do
+      let(:number) { 15 }
+
+      it 'returns "FizzBuzz"' do
+        expect(call).to eq('FizzBuzz')
+      end
+    end
+
+    context 'when #number is divisible not by 5' do
+      let(:number) {3}
+
+      it 'returns "Fizz"' do
+        expect(call).to eq('Fizz')
+      end
+    end
+  end
+
+  context 'when #number is not divisible by 3' do
+    context 'when #number is divisible by 5' do
+      let(:number) { 5 }
+
+      it 'returns "Buzz"' do
+        expect(call).to eq('Buzz')
+      end
+    end
+
+    context 'when #number is divisible not by 5' do
+      let(:number) {4}
+
+      it 'returns an empty string' do
+        expect(call).to eq('')
+      end
+    end
+  end
+end
